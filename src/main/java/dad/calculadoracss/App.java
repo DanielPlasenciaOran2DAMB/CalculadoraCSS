2c85b1313b9a1773866b0ba4a8ebd74761933b08
DanielPlasenciaOran2DAMB <2bacdanielplasencia@gmail.com>
2022-01-23 14:43:10 +0000

    Initial commit

---
+++ src/main/java/dad/calculadoracss/App.java
@@ -0,0 +1,28 @@
+package dad.calculadoracss;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+	CalculadoraController controller;
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		controller = new CalculadoraController();
+
+		Scene scene = new Scene(controller.getView());
+
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Calculadora");
+		primaryStage.getIcons().add(new Image("/img/calculator-32x32.png"));
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
